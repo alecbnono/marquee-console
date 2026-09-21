51ddeb90a755f967f0361653193cb52c4c7447c4
PrinceMPS <prinmat88@gmail.com>
2026-09-21 22:55:22 +0800

    feat/add member names

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@ int main() {
     View view;
     Controller controller(model, view);
 
+    view.showMessage("Welcome to CSOPESY!.\n");
+    view.showMessage("Group Developers:");
+    view.showMessage("Nono, Alec Marx");
+    view.showMessage("Ponce, Jean Rondel");
+    view.showMessage("Sy, Prince Matthew\n");
     view.showMessage("Type 'help' for a list of commands.");
 
     std::string line;
